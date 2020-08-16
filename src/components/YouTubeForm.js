55f836c8f1da3b26d16f34d8df63814a6fa97a89
Americanman <snowboardracer7@gmail.com>
2020-08-16 04:45:58 -0400

    Set up Formik to handle/track changes of input fields

--- src/components/YouTubeForm.js
+++ src/components/YouTubeForm.js
@@ -1,18 +1,29 @@
 import React from 'react'
+import { useFormik } from 'formik'
 
 export default function YouTubeForm() {
+	const formik = useFormik({
+		initialValues: {
+			name: '',
+			email: '',
+			channel: ''
+		}
+	})
+	 
+console.log("Form values", formik.values)
+
 	return (
 		<div>
 			<form>
 				<label>Name</label>
-				<input type="text" name="name" id="name"/>
+				<input type="text" name="name" id="name" onChange={formik.handleChange} value={formik.values.name} placeholder='yoo' />
 
 				<label htmlFor="email">E-mail</label>
-				<input type="text" name="email" id="email"/>
+				<input type="text" name="email" id="email" onChange={formik.handleChange} value={formik.values.email}/>
 
 				<label htmlFor="channel">channel</label>
-				<input type="text" name="channel" id="channel"/>
-				<button type="submit">Submit</button>
+				<input type="text" name="channel" id="channel" onChange={formik.handleChange} value={formik.values.channel}/>
+				<button >Submit</button>
 			</form>
 		</div>
 	)
